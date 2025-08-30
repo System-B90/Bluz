c39f294145fd75929521d7fd7a72c0bbd94ce8bc
Michael K. Steinberg <m.kuper.steinberg@gmail.com>
2025-08-30 23:25:55 +0300

    Vibe coded the living shit out of this project

--- src/app/(themed)/(post-auth)/schedule/layout.tsx
+++ src/app/(themed)/(post-auth)/schedule/layout.tsx
@@ -1,15 +1,13 @@
-
+import React from 'react';
 
 export default async function ScheduleLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
-}>)
-{
-
+}>) {
     return (
-        <div>
-            { children }
+        <div className="schedule-layout">
+            {children}
         </div>
     );
 }
