5c200f939af37b1d61e9fdcd345add42ad15c70e
Michael K. Steinberg <m.kuper.steinberg@gmail.com>
2026-02-22 23:07:46 +0200

    Changed color

--- src/components/header/filter-icon.tsx
+++ src/components/header/filter-icon.tsx
@@ -13,7 +13,7 @@ export default function FilterIcon({ filtersVisible, setFiltersVisible }: { filt
         <Tooltip title={ `${filtersVisible ? 'Hide' : 'Show'} Filters` } placement='bottom'>
             <IconButton className='relative' color="inherit" onClick={ () => { setFiltersVisible(v => !v); } }>
                 <FilterListIcon color={ filtersVisible ? 'primary' : 'inherit' } />
-                { (!filtersVisible && hasAnyFilter) && <Tooltip placement='right' title='יש סננים נסתרים'><InfoIcon className='absolute top-0.5 right-0.5 opacity-80' fontSize='inherit' sx={ { fontSize: '1.1rem' } } /></Tooltip> }
+                { (!filtersVisible && hasAnyFilter) && <Tooltip placement='right' title='יש סננים נסתרים'><InfoIcon className='absolute top-0.5 right-0.5' color='info' fontSize='inherit' sx={ { fontSize: '1.1rem' } } /></Tooltip> }
             </IconButton>
         </Tooltip>
     );
