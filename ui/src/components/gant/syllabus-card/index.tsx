8960d6d3e97c3e46341e71700d194140c47e6a9f
Michael K. Steinberg <m.kuper.steinberg@gmail.com>
2026-04-13 18:14:07 +0300

    Merged syllabus card parts

--- ui/src/components/gant/syllabus-card/index.tsx
+++ ui/src/components/gant/syllabus-card/index.tsx
@@ -1,16 +1,23 @@
 import { SyllabusId } from "@/api-shared/types/gant/curriculum";
-import { Card, CardHeader, Typography } from '@mui/material';
-import { SyllabusCardInner } from './SyllabusCardInner';
+import { useSyllabus } from '@/components/gant/state/hooks';
+import { Card, CardContent, CardHeader, Typography } from '@mui/material';
+import { ModulesTable } from './ModulesTable';
+import { SyllabusName } from './SyllabusName';
 
 export default function SyllabusCard({ syllabusId }: { syllabusId: SyllabusId; })
 {
+    const syllabus = useSyllabus(syllabusId);
+
     return (
         <Card sx={ { display: 'flex', flexDirection: 'column', width: '30%', minWidth: 350, maxHeight: '90%', overflow: 'hidden' } }>
             <CardHeader
                 title={ <Typography variant="subtitle2" color="textSecondary">סילבוס</Typography> }
                 sx={ { pb: 0, pt: 1.5, px: 2 } }
             />
-            <SyllabusCardInner syllabusId={ syllabusId } />
+            <CardContent sx={ { display: 'flex', flexDirection: 'column', paddingY: 1, flex: 1, overflow: 'hidden' } }>
+                <SyllabusName key={ `${syllabus?.title ?? '-syllabus-title'}` } syllabusId={ syllabusId } />
+                <ModulesTable syllabusModules={ syllabus?.modules ?? [] } syllabusId={ syllabusId } />
+            </CardContent>
         </Card>
     );
 }
