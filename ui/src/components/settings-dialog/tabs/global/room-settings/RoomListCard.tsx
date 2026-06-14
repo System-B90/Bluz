97a8363572d5b315969136ca092f7df8ac9305d0
Michael K. Steinberg <m.kuper.steinberg@gmail.com>
2026-06-15 02:02:40 +0300

    Resolved deprecated inputProps and InputProps APIs in favor of slotProps

--- ui/src/components/settings-dialog/tabs/global/room-settings/RoomListCard.tsx
+++ ui/src/components/settings-dialog/tabs/global/room-settings/RoomListCard.tsx
@@ -103,19 +103,21 @@ export function RoomListCard({
             <RoomListHeader />
 
             <TextField
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <SearchIcon
-                                fontSize="small"
-                                sx={{ color: "text.secondary" }}
-                            />
-                        </InputAdornment>
-                    ),
-                }}
                 onChange={(e) => setSearchQuery(e.target.value)}
                 placeholder="חפש חדר..."
                 size="small"
+                slotProps={{
+                    input: {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon
+                                    fontSize="small"
+                                    sx={{ color: "text.secondary" }}
+                                />
+                            </InputAdornment>
+                        ),
+                    },
+                }}
                 sx={{
                     "& .MuiOutlinedInput-root": {
                         borderRadius: "10px",
